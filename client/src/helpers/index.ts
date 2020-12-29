a57aeaab337564b4cae575defe4cb0add433bc03
Matt Hurst <matt.adam.hurst@gmail.com>
2020-12-29 12:33:42 +0000

    feat(client): add login function to helpers file

--- client/src/helpers/index.ts
+++ client/src/helpers/index.ts
@@ -1,4 +1,15 @@
 import { Book } from '../interfaces'
+import axios from 'axios'
+
+export const login = async (name: string, password: string): Promise<string> => {
+  const { data } = await axios.post(`${URL}availableBooks`, {
+    name,
+    password
+  })
+  console.log('LOGIN FUNCTION IN HELPER FOLDER DATA: ', data )
+  localStorage.setItem('token', data.token)
+  return data;
+}
 
 export const calculateBooksReadThisYear = (books: Book[]) => {
   
