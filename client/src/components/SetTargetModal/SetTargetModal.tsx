f3bcc3d1ccfc60ae053d37758072910ee64b5c9f
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-07 19:34:01 +0000

    feat(client): add styling to edit target modal

--- client/src/components/SetTargetModal/SetTargetModal.tsx
+++ client/src/components/SetTargetModal/SetTargetModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { AiFillCloseCircle } from 'react-icons/ai';
 
+import './SetTargetModal.scss'
+
 interface SetTargetModalProps {
   turnModalOff: Function;
   changeTarget: Function;
@@ -15,26 +17,37 @@ const SetTargetModal: React.FC<SetTargetModalProps> = ({ turnModalOff, changeTar
   }
 
   return (
-    <div>
-      <div data-testid="greyedBackgroundDiv" onClick={() => turnModalOff()}></div>
-      <AiFillCloseCircle onClick={() => turnModalOff()} data-testid="closeSVG" />
-      <h2>Set new target</h2>
-      <form
-        action="submit"
-        onSubmit={
-          async (event) => {
-            event.preventDefault()
-            if (input === '') turnModalOff()
-            else {
-              await changeTarget(+input)
-              turnModalOff()
+    <div className='edit-target-modal-grand-wrapper'>
+      <div 
+        data-testid="greyedBackgroundDiv" 
+        className='edit-target-modal-grand-wrapper__greyed-background-div'
+        onClick={() => turnModalOff()}>
+      </div>
+      <div className='edit-target-modal-grand-wrapper__pop-out-container'>
+        <AiFillCloseCircle 
+          data-testid="closeSVG" 
+          className='edit-target-modal-grand-wrapper__pop-out-container__close-btn'
+          onClick={() => turnModalOff()} 
+        />
+        <h1>Set new target</h1>
+        <form
+          action="submit"
+          onSubmit={
+            async (event) => {
+              event.preventDefault()
+              if (input === '') turnModalOff()
+              else {
+                await changeTarget(+input)
+                turnModalOff()
+              }
             }
-          }
-        }>
-        <input value={input} type="number" onChange={handleChange} />
-        <button type="submit">submit</button>
-      </form>
-    </div>
+          }>
+          <input value={input} type="number" onChange={handleChange} />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+
+      </div>
   )
 }
 
